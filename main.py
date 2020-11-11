2e96014adcc83a008ced03a0c1853f60fb83ddc1
bonom <andrea.bonomi489@gmail.com>
2020-11-11 14:48:43 +0100

    Implemented algorithm

--- main.py
+++ main.py
@@ -13,7 +13,8 @@
 
 import numpy as np
 import time
-import d_wave
+import Matrix_Optimization
+import dwave_networkx as dnx
 import matplotlib.pyplot as plt
 import networkx as nx
 import sys
@@ -89,6 +90,59 @@ def h(vect, pr):  # algorithm 4
         if make_decision(pr):
             vect[i] = -vect[i]
     return vect
+
+def g_faster(Q, A, oldperm, pr, P):
+    n, cols = Q.shape
+    m = dict()
+    for i in range(n):
+        if make_decision(pr):
+            m[i] = i
+    vals = list(m.values())
+    np.random.shuffle(vals)
+    m = dict(zip(m.keys(), vals))
+    Pprime = np.empty((n, n), dtype=int)
+    for i in range(n):
+        if i in m.values():
+            Pprime[i] = P[m[i]]
+        else:
+            Pprime[i] = P[i]
+
+    perm = fill(m, oldperm, n)
+    inversed = inverse(perm, n)
+
+    Theta = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            if (A.item(i, j) == 1):
+                k = inversed[i]
+                l = inversed[j]
+                Theta[i][j] = Q[k][l]
+
+    tmp = np.multiply((((np.transpose(Pprime)).dot(Q)).dot(Pprime)), (A))
+
+    if (tmp == Theta).all() == False:
+        print("Error calculating Theta via algorithm")
+        exit(-1)
+
+    return Pprime, Theta, perm
+
+
+def fill(m, perm, n):
+    filled = np.empty((n), dtype=int)
+    for i in range(n):
+        if i in m:
+            filled[i] = perm[m[i]]
+        else:
+            filled[i] = perm[i]
+    return filled
+
+
+def inverse(perm, n):
+    inverted = np.empty((n), dtype=int)
+    for i in range(n):
+        inverted[perm[i]] = i
+
+    return inverted
 
 
 def QALS(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, A, Q):
@@ -171,6 +225,83 @@ def QALS(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, A, Q):
         
     return z_star
 
+def QALS_g(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, A, Q):
+    I = np.identity(n)
+    P = I
+    p = 1
+    P_one, Theta_one, m_one = g_faster(Q, A, np.arange(n), p, P)
+    P_two, Theta_two, m_two = g_faster(Q, A, np.arange(n), p, P)
+    # for i in range(k):
+    z_one = (np.transpose(P_one)).dot(minimization(Theta_one))
+    z_two = (np.transpose(P_two)).dot(minimization(Theta_two))
+    f_one = function_f(Q, z_one)
+    f_two = function_f(Q, z_two)
+    if (f_one < f_two).all():
+        z_star = z_one
+        f_star = f_one
+        P_star = P_one
+        m_star = m_one
+        z_prime = z_two
+    else:
+        z_star = z_two
+        f_star = f_two
+        P_star = P_two
+        m_star = m_two
+        z_prime = z_one
+    if (f_one == f_two).all() == False:
+        S = (np.outer(z_prime, z_prime) - I) + np.diagflat(z_prime)
+    else:
+        S = np.zeros((n, n))
+    e = 0
+    d = 0
+    i = 0
+    lam = lambda_zero
+    while True:
+        print(f"-- Ciclo numero {i + 1}")  # , end = "\r")
+        Q_prime = np.add(Q, (np.multiply(lam, S)))
+        if (i % n == 0):
+            p = p - ((p - p_delta)*eta)
+        P, Theta_prime, m = g_faster(Q_prime, A, m_star, p, P_star)
+        # for i in range(k):
+        z_prime = (np.transpose(P)).dot(minimization(Theta_prime))
+        sys.stdout.write("\033[K\033[F\033[K")
+        if make_decision(q):
+            z_prime = h(z_prime, q)
+        if (z_prime == z_star).all() == False:
+            f_prime = function_f(Q_prime, z_prime)
+            if (f_prime < f_star).all():
+                z_prime, z_star = z_star, z_prime
+                f_star = f_prime
+                P_star = P
+                m_star = m
+                e = 0
+                d = 0
+                S = S + ((np.outer(z_prime, z_prime) - I) +
+                         np.diagflat(z_prime))
+            else:
+                d = d + 1
+                if make_decision((p**(f_prime-f_star))):
+                    z_prime, z_star = z_star, z_prime
+                    f_star = f_prime
+                    P_star = P
+                    m_star = m
+                    e = 0
+            # R:37 lambda diminuirebbe
+            # lam = lam - i/i_max
+        else:
+            e = e + 1
+        i = i + 1
+        if ((i == i_max) or ((e + d >= N_max) and (d < d_min))):
+            sys.stdout.write("\033[K")
+            print(f"Uscito al ciclo {i}/{i_max} ", end = '')
+            if(i != i_max):
+                print("ed è stata raggiunta la convergenza.")
+            else:
+                print("\n")
+            break
+        
+    return z_star
+
 def show_graph(adjacency_matrix, mylabels=None):
     rows, cols = np.where(adjacency_matrix == 1)
     edges = zip(rows.tolist(), cols.tolist())
@@ -214,7 +345,7 @@ def main():
     print(f"\nCreo A ...", end=' ')
     
     if(n % 8 == 0) and (rows * columns * 8 == n):
-        A = d_wave.chimera_graph(rows, columns)
+        A = dnx.chimera_graph(rows, columns)
         matrix_A = (nx.adjacency_matrix(A)).todense()
     else:
         exit("Error", -1)
@@ -225,11 +356,18 @@ def main():
         show_graph(matrix_A)
 
     print("\n")
+
+    #start_time = time.time()
+    #
+    #QALS(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, matrix_A, Q)
+    #
+    #print("\n------------ Impiegati %0.2f secondi senza l'algoritmo ------------\n\n" %
+    #      (time.time() - start_time))
     start_time = time.time()
     
-    QALS(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, matrix_A, Q)
+    QALS_g(d_min, eta, i_max, k, lambda_zero, n, N_max, p_delta, q, matrix_A, Q)
     
-    print("\n------------ Impiegati %0.2f secondi ------------\n\n" %
+    print("\n------------ Impiegati %0.2f secondi con l'algoritmo ------------\n\n" %
           (time.time() - start_time))
 
 
