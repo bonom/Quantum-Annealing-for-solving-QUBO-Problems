65e62247c1e852bf8fc037912aa1c15ca1b62a20
Andrea Bonomi <andrea.bonomi489@gmail.com>
2020-12-18 10:15:03 +0000

    Update

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 clear
-pip3 install -r requirements.txt | grep "not installed" 
+#pip3 install -r requirements.txt | grep "not installed" 
 
 [ ! -d "output/" ] && mkdir output/
-
 
 python3 -B start.py $1
