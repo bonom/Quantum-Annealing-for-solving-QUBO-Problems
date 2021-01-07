7670fda3368737ea546068c02bb3a3b8d4e72fd2
Andrea Bonomi <andrea.bonomi489@gmail.com>
2021-01-07 08:43:21 +0000

    Final

--- QA4QUBO/solver.py
+++ QA4QUBO/solver.py
@@ -248,7 +248,7 @@ def solve(d_min, eta, i_max, k, lambda_zero, n, N, N_max, p_delta, q, A, Q, DIR)
             if make_decision(q):
                 z_prime = h(z_prime, q)
 
-            if (z_prime == z_star).all() == False:
+            if (z_prime == z_star) == False:
                 f_prime = function_f(Q, z_prime).item()
                 if (f_prime < f_star):
                     z_prime, z_star = z_star, z_prime
